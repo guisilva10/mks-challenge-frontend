5e8f9de79352eacddfbb0d82b70ae6cc9dd0460a
Guilherme Lopes <guisilva.070104@gmail.com>
2024-04-26 02:52:44 -0300

    BREAKING CHANGE: add cart component and products list

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Product from "@/types/products";
 import Footer from "./_components/Footer/footer.component"
 import Header from "./_components/Header/header.component"
 import { ProductsList } from "./_components/ProductsList/productslist.component"
 import Cart from "./_components/cart/cart.component";
-interface Product {
+interface ProductProps {
   id: string;
   name: string;
   price: number;
@@ -10,11 +11,9 @@ interface Product {
   photo: string;
   quantity: number;
 }
-
 interface HeaderProps {
   products: Product;
 }
-
 
 export default async function Home({ products}: HeaderProps) {
 
